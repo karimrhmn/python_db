ff0a36638b45f500a1d6d2a803396ab9eb402c30
karimrhmn <163521992+karimrhmn@users.noreply.github.com>
2024-10-18 17:38:14 +0100

    Functions update
    More or less the same code just made a little cleaner and 'future proof' by implementing the use of functions!

--- pydata.py
+++ pydata.py
@@ -26,26 +26,9 @@ conn.close()
 
 
 # Function - View the database
-#def view_db():
-
-# Function - Add an entry
-#def add_entry():
-
-# Function - Delete an entry
-#def del_entry():
-
-# Bool to trigger the program on and off
-program = True
-
-
-
-# Starting the program, and the main menue
-while program:
-    int_input = int(input("\nView the database: 1,\nAdd an entry: 2,\nRemove an entry: 3\nExit the program: 4\n:"))
-
-    if int_input == 1:
-
-        # Reconnect to the server
+def view_db():
+    
+  # connect to the server
         conn = sqlite3.connect("data.db")
         cursor = conn.cursor()
         
@@ -60,71 +43,93 @@ while program:
         # Close the connection
         conn.close()
 
-
-    if int_input == 2:
-
+# Function - Add an entry
+def add_entry():
+        
+        # Gather user input
         user_name = input("Enter your first name!: ")
         user_age = int(input("Enter your age!: "))
         user_occ = input("What's your occupation!: ")
 
 
-        # Reconnect to the server
+        # Connect to the server
         conn = sqlite3.connect("data.db")
         cursor = conn.cursor()
 
         # Add entry
         cursor.execute('''
-         INSERT INTO people (name, age, occupation) 
-          VALUES (?, ?, ?)
+        INSERT INTO people (name, age, occupation) 
+        VALUES (?, ?, ?)
         ''', (user_name, user_age, user_occ))
         
+        # Close and commit changes
         conn.commit()
         conn.close()
 
-    if int_input == 3:
+# Function - Delete an entry
+def del_entry():
+    
+    user_prompt = input("Would you like to delete by name or by id?\nReply with 'name' or 'id'! ")
 
-        user_prompt = input("Would you like to delete by name or by id?\nReply with 'name' or 'id'! ")
-
-        if user_prompt == "name":
-
-            #Gather name from user
-            del_request = input("Enter the name you'd like to delete: ")
+    if user_prompt == "name":
+        #Gather name from user
+        del_request = input("Enter the name you'd like to delete: ")
 
 
-            # Connect to the server
-            conn = sqlite3.connect("data.db")
-            cursor = conn.cursor()
+        # Connect to the server
+        conn = sqlite3.connect("data.db")
+        cursor = conn.cursor()
 
-            # Delete the chosen user
-            cursor.execute('''
-            DELETE FROM people WHERE name = ?
-            ''', (del_request,))
+        # Delete the chosen user
+        cursor.execute('''
+        DELETE FROM people WHERE name = ?
+        ''', (del_request,))
 
-            # Commit changes and close server
-            conn.commit()
-            conn.close()
+        # Commit changes and close server
+        conn.commit()
+        conn.close()
         
-        elif user_prompt == "id":
+    elif user_prompt == "id":
 
-            #Gather id from user
-            del_request = int(input("Enter the id you'd like to delete: "))
+        #Gather id from user
+        del_request = int(input("Enter the id you'd like to delete: "))
 
-            # Connect to the server
-            conn = sqlite3.connect("data.db")
-            cursor = conn.cursor()
+        # Connect to the server
+        conn = sqlite3.connect("data.db")
+        cursor = conn.cursor()
 
-            # Delete the chosen user
-            cursor.execute('DELETE FROM people WHERE id = ?', (del_request,))
+        # Delete the chosen user
+        cursor.execute('DELETE FROM people WHERE id = ?', (del_request,))
 
-            # Commit changes and close server
-            conn.commit()
-            conn.close()
-        else:
-            print()
-            print("Sorry you need to type either 'name' or 'id?")
+        # Commit changes and close server
+        conn.commit()
+        conn.close()
+    else:
+        print()
+        print("Sorry you need to type either 'name' or 'id?")
 
 
-    if int_input == 4:
+
+# Bool to trigger the program on and off
+program = True
+
+
+
+# Starting the program, and the main menue
+while program:
+    
+    int_input = int(input("\nView the database: 1,\nAdd an entry: 2,\nRemove an entry: 3\nExit the program: 4\n:"))
+
+    if int_input == 1:
+        view_db()
+
+    elif int_input == 2:
+        add_entry()
+
+    elif int_input == 3:
+        del_entry()
+
+    elif int_input == 4:
         program = False
 
 #Next update will make use of the functions in python
